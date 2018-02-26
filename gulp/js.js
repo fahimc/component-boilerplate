f1f31bcbf70f6a7f56eb682337ea81251230ce73
fahim chowdhury <fahim@gmx.com>
2018-02-26 10:48:41 +0000

    clean up

--- gulp/js.js
+++ gulp/js.js
@@ -7,26 +7,11 @@ var babelify = require('babelify');
 
 module.exports = function() {
     gulp.task('js', function() {
-        /*
-        return gulp.src(['src/app.js'])
-            .pipe(concat('app.js'))
-            .pipe(browserify({ transform: ['babelify'] }))
-            .pipe(minify({
-                ext: {
-                    src: '.js',
-                    min: '.min.js'
-                },
-                exclude: ['tasks'],
-                ignoreFiles: ['.combo.js', '-min.js']
-            }))
-            .pipe(gulp.dest('dist/js'));
-            */
         return browserify('src/app.js')
             .transform(babelify.configure({
                 presets: ["es2015"]
             }))
             .bundle()
             .pipe(fs.createWriteStream('dist/js/app.js'));
-        //.pipe(gulp.dest('dist/js'));
     });
 }
